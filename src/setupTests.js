f4652e38af4f00a93e52ca26279b721392456a0f
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-07-17 14:26:19 +0200

    List all repositories and implement  functionality

---
+++ src/setupTests.js
@@ -0,0 +1,4 @@
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+// eslint-disable-next-line no-undef
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
